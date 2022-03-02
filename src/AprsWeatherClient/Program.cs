08ce22b40441b2ac4d909c3005ce23d3da7aa75c
Cameron Bielstein <ctbielstein@gmail.com>
2022-03-01 22:27:28 -0800

    Prepare for multi-project structure.

--- src/AprsWeatherClient/Program.cs
+++ src/AprsWeatherClient/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AprsWeather;
+using AprsWeatherClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
