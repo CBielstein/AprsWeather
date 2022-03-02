7d1c35a097bfa24443fa821677bbc7916b1b8c01
Cameron Bielstein <ctbielstein@gmail.com>
2022-03-01 23:51:22 -0800

    Now receiving APRS-IS packets!

--- src/AprsWeatherServer/BackgroundServices/AprsIsReceiver.cs
+++ src/AprsWeatherServer/BackgroundServices/AprsIsReceiver.cs
@@ -1,41 +1,38 @@
+using AprsSharp.Connections.AprsIs;
+using AprsSharp.Parsers.Aprs;
+
 namespace AprsWeatherServer.BackgroundServices;
 
 public class AprsIsReceiver: IHostedService
 {
     private readonly IDictionary<string, string> reports = new Dictionary<string, string>();
-    private bool receiving = false;
     private Task? receiveTask;
+    private readonly AprsIsConnection client = new AprsIsConnection(new TcpConnection());
 
     public AprsIsReceiver(IDictionary<string, string> reports)
     {
         this.reports = reports;
+        client.ReceivedPacket += p =>
+        {
+            if (p.InfoField is WeatherInfo)
+            {
+                reports[p.Sender] = p.Encode();
+            }
+        };
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        receiving = true;
-        receiveTask = Receive();
+        string callsign = Environment.GetEnvironmentVariable("APRS_IS_CALLSIGN") ?? throw new ArgumentException("APRS_IS_CALLSIGN environment variable must be set.");
+
+        // Return any packets (less non-position types) within 50 km of Seattle's Space Needle
+        receiveTask = client.Receive(callsign, "-1", "noam.aprs2.net", "r/47.620157/-122.349643/50 -t/oimqstu");
+
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        receiving = false;
         return receiveTask ?? Task.CompletedTask;
-    }
-
-    private async Task Receive()
-    {
-        int count = 0;
-        while (receiving)
-        {
-            Thread.Sleep(5000);
-
-            ++count;
-            Console.WriteLine(count);
-            reports.Add(count.ToString(), count.ToString());
-
-            await Task.Yield();
-        }
     }
 }
