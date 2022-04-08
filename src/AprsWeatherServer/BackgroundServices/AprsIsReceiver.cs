bb2e266c857f671fd41c87a62d45451b93b7a234
Cameron Bielstein <ctbielstein@gmail.com>
2022-04-08 01:12:14 -0700

    Refactor AprsIsReceiver to allow reconnection and stopping.

--- src/AprsWeatherServer/BackgroundServices/AprsIsReceiver.cs
+++ src/AprsWeatherServer/BackgroundServices/AprsIsReceiver.cs
@@ -7,38 +7,82 @@ namespace AprsWeatherServer.BackgroundServices;
 public class AprsIsReceiver: IHostedService
 {
     private readonly IDictionary<string, WeatherReport<string>> reports;
+    private readonly ILogger<AprsIsReceiver> logger;
     private Task? receiveTask;
-    private readonly AprsIsConnection client = new AprsIsConnection(new TcpConnection());
+    private TcpConnection? tcpConnection;
+    private AprsIsConnection? client;
+    private bool attemptReconnect = true;
+    private readonly string callsign = Environment.GetEnvironmentVariable("APRS_IS_CALLSIGN")
+        ?? throw new ArgumentException("APRS_IS_CALLSIGN environment variable must be set.");
+    private const string password = "-1";
+    private const string server = "noam.aprs2.net";
 
-    public AprsIsReceiver(IDictionary<string, WeatherReport<string>> reports)
+    // Return any packets (less non-position types) within 50 km of Seattle's Space Needle
+    private const string filter = "r/47.620157/-122.349643/50 -t/oimqstu";
+
+    public AprsIsReceiver(
+        IDictionary<string, WeatherReport<string>> reports,
+        ILogger<AprsIsReceiver> logger)
     {
         this.reports = reports;
+        this.logger = logger;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        string callsign = Environment.GetEnvironmentVariable("APRS_IS_CALLSIGN") ?? throw new ArgumentException("APRS_IS_CALLSIGN environment variable must be set.");
-
-        client.ReceivedPacket += p =>
-        {
-            if (p.InfoField is WeatherInfo)
-            {
-                reports[p.Sender] = new WeatherReport<string>()
-                {
-                    ReceivedTime = DateTimeOffset.UtcNow,
-                    Report = p.Encode(),
-                };
-            }
-        };
-
-        // Return any packets (less non-position types) within 50 km of Seattle's Space Needle
-        receiveTask = client.Receive(callsign, "-1", "noam.aprs2.net", "r/47.620157/-122.349643/50 -t/oimqstu");
-
+        logger.LogInformation("Starting service: {serviceName}", nameof(AprsIsReceiver));
+        CreateConnection(ConnectionState.Disconnected);
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        logger.LogInformation("Stopping service: {serviceName}", nameof(AprsIsReceiver));
+        attemptReconnect = false;
+        tcpConnection?.Dispose();
         return receiveTask ?? Task.CompletedTask;
+    }
+
+    private void CreateConnection(ConnectionState newState)
+    {
+        logger.LogInformation("APRS-IS connection entered new state: {state}", newState);
+
+        if (client == null || newState == ConnectionState.Disconnected)
+        {
+            if (client != null && newState == ConnectionState.Disconnected)
+            {
+                logger.LogWarning("APRS-IS connection failed.");
+                Thread.Sleep(15000);
+            }
+
+            if (!attemptReconnect)
+            {
+                logger.LogInformation("Not attempting APRS-IS reconnection.");
+                return;
+            }
+
+            logger.LogInformation("Creating new {AprsIsClient}", nameof(AprsIsConnection));
+
+            tcpConnection?.Dispose();
+            tcpConnection = new TcpConnection();
+            client = new AprsIsConnection(tcpConnection);
+
+            client.ReceivedPacket += StorePacket;
+            client.ChangedState += CreateConnection;
+
+            receiveTask = client.Receive(callsign, password, server, filter);
+        }
+    }
+
+    private void StorePacket(Packet p)
+    {
+        if (p.InfoField is WeatherInfo)
+        {
+            reports[p.Sender] = new WeatherReport<string>()
+            {
+                ReceivedTime = DateTimeOffset.UtcNow,
+                Report = p.Encode(),
+            };
+        }
     }
 }
