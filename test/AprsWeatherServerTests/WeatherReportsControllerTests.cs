f39431c0463dc0d7cf4b73f55ee90c84022ac796
Cameron Bielstein <ctbielstein@gmail.com>
2022-05-07 21:08:37 -0700

    Added stub tests.

---
+++ test/AprsWeatherServerTests/WeatherReportsControllerTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace AprsWeatherServerTests;
+
+public class WeatherReportsControllerTests
+{
+    /// <summary>
+    /// Verifies all reports are returned
+    /// </summary>
+    [Fact]
+    public void TestGetAllReports()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Verifies empty list when no reports available
+    /// </summary>
+    [Fact]
+    public void TestGetAllReportsEmpty()
+    {
+        throw new System.NotImplementedException();
+    }
+}
