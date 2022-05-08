85f4afd1094420a2f6f41842bb2f8b1a33c7ccc0
Cameron Bielstein <ctbielstein@gmail.com>
2022-05-07 21:35:21 -0700

    Added test infrastructure.

--- test/AprsWeatherServerTests/WeatherReportsControllerTests.cs
+++ test/AprsWeatherServerTests/WeatherReportsControllerTests.cs
@@ -1,9 +1,28 @@
+using System.Collections.Generic;
+using AprsWeather.Shared;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace AprsWeatherServerTests;
 
 public class WeatherReportsControllerTests
 {
+    public readonly IDictionary<string, WeatherReport<string>> reports = new Dictionary<string, WeatherReport<string>>();
+
+    public WeatherReportsControllerTests()
+    {
+        var app = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(b =>
+            {
+                b.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IDictionary<string, WeatherReport<string>>>(reports);
+                });
+            });
+    }
+
     /// <summary>
     /// Verifies all reports are returned
     /// </summary>
