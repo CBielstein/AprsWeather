91cd470e2299dda75a339fa03d9688617d4d1415
Cameron Bielstein <ctbielstein@gmail.com>
2022-05-09 22:08:56 -0700

    Implement empty response test.

--- test/AprsWeatherServerTests/WeatherReportsControllerTests.cs
+++ test/AprsWeatherServerTests/WeatherReportsControllerTests.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using AprsSharp.Parsers.Aprs;
 using AprsWeather.Shared;
 using AprsWeatherServer.BackgroundServices;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -12,7 +16,7 @@ namespace AprsWeatherServerTests;
 
 public class WeatherReportsControllerTests
 {
-    public readonly IDictionary<string, WeatherReport<string>> reports = new Dictionary<string, WeatherReport<string>>();
+    public readonly IDictionary<string, WeatherReport<string>> serverReports = new Dictionary<string, WeatherReport<string>>();
     public readonly HttpClient client;
 
     public WeatherReportsControllerTests()
@@ -23,7 +27,7 @@ public class WeatherReportsControllerTests
                 builder
                     .ConfigureTestServices(services =>
                     {
-                        services.AddSingleton(reports);
+                        services.AddSingleton(serverReports);
 
                         var receiver = services.Single(s => s.ImplementationType == typeof(AprsIsReceiver));
                         services.Remove(receiver);
@@ -50,8 +54,33 @@ public class WeatherReportsControllerTests
     /// Verifies empty list when no reports available
     /// </summary>
     [Fact]
-    public void TestGetAllReportsEmpty()
+    public async Task TestGetAllReportsEmpty()
     {
-        throw new System.NotImplementedException();
+        serverReports.Clear();
+
+        var reports = await GetReports();
+        Assert.Empty(reports);
+    }
+
+    /// <summary>
+    /// Gets <see cref="WeatherReport"/>s from the test server
+    /// </summary>
+    /// <param name="expected"></param>
+    /// <returns>List of <see cref="WeatherReport"/>.</returns>
+    private async Task<IEnumerable<WeatherReport<Packet>>> GetReports(HttpStatusCode expectedStatus = HttpStatusCode.OK)
+    {
+        // var response = await client.GetAsync<IEnumerable<WeatherReport<string>>>("/WeatherReports");
+        var response = await client.GetAsync("/WeatherReports");
+        Assert.Equal(expectedStatus, response.StatusCode);
+
+        var reports = await response.Content.ReadFromJsonAsync<IEnumerable<WeatherReport<string>>>()
+            ?? Enumerable.Empty<WeatherReport<string>>();
+
+        return reports.Select(r =>
+            new WeatherReport<Packet>()
+            {
+                Report = new Packet(r.Report),
+                ReceivedTime = r.ReceivedTime,
+            });
     }
 }
