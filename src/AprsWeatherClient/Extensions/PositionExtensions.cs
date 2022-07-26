ea9fb3ff313dd3f04d7a885d3775c624f455dadb
Cameron Bielstein <CTBielstein@gmail.com>
2022-07-25 23:23:03 -0700

    Show closest report based on user input (#16)
    This PR switches from showing _all_ reports to showing the closest report to a user based on their manual input of a gridsquare.
    
    This does the following:
    * Adds a location box and update button at the top
    * Adds logic to UI to show single report for more metrics
    
    Distance and heading to report and calculated by the Geolocation nuget package.

---
+++ src/AprsWeatherClient/Extensions/PositionExtensions.cs
@@ -0,0 +1,50 @@
+using AprsSharp.Parsers.Aprs;
+using Geolocation;
+
+namespace AprsWeatherClient.Extensions;
+
+/// <summary>
+/// Extenion methods for the <see cref="Position"/> class.
+/// </summary>
+public static class PositionExtensions
+{
+    /// <summary>
+    /// Calculates a distance from a user position to a report position
+    /// </summary>
+    /// <param name="userPositions"><see cref="Position"/> of the user</param>
+    /// <param name="reportPosition"><see cref="Position"/> of the report</param>
+    /// <returns>Distance in miles</returns>
+    public static double? MilesTo(this Position? userPosition, Position reportPosition)
+    {
+        if (userPosition == null)
+        {
+            return null;
+        }
+
+        return GeoCalculator.GetDistance(
+            userPosition.Coordinates.Latitude,
+            userPosition.Coordinates.Longitude,
+            reportPosition.Coordinates.Latitude,
+            reportPosition.Coordinates.Longitude);
+    }
+
+    /// <summary>
+    /// Calculates a direction from a user position to a report position
+    /// </summary>
+    /// <param name="userPositions"><see cref="Position"/> of the user</param>
+    /// <param name="reportPosition"><see cref="Position"/> of the report</param>
+    /// <returns>Direction string (e.g. NW, E, etc.)</returns>
+    public static string? DirectionTo(this Position? userPosition, Position reportPosition)
+    {
+        if (userPosition == null)
+        {
+            return null;
+        }
+
+        return GeoCalculator.GetDirection(
+            userPosition.Coordinates.Latitude,
+            userPosition.Coordinates.Longitude,
+            reportPosition.Coordinates.Latitude,
+            reportPosition.Coordinates.Longitude);
+    }
+}
