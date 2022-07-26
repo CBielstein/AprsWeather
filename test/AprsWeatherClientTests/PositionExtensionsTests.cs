89e1a65b62af9c3d396e614aabe28216ff730b8b
Cameron Bielstein <ctbielstein@gmail.com>
2022-07-25 21:24:27 -0700

    Add client tests for Position extensions.

---
+++ test/AprsWeatherClientTests/PositionExtensionsTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AprsWeatherServerTests;
+
+public class PositionExtensionsTests
+{
+    [Fact]
+    public void NotImplemented()
+    {
+        throw new System.NotImplementedException();
+    }
+}
