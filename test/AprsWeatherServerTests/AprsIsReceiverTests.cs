cbd1b07f72e649a71ef83fa91bf5a255b8225d78
Cameron Bielstein <ctbielstein@gmail.com>
2022-12-29 17:45:11 -0800

    Additional test data.

--- test/AprsWeatherServerTests/AprsIsReceiverTests.cs
+++ test/AprsWeatherServerTests/AprsIsReceiverTests.cs
@@ -20,6 +20,10 @@ public class AprsIsReceiverTests
     /// <param name="expectedInclude">Expected include result</param>
     [Theory]
     [InlineData(@"N0CALL>WIDE1-1,WIDE2-2:/092345z4903.50N/07201.75W_180/010g015t068r001p011P010h99b09901l010#010s050 Testing WX packet.", true)]
+    [InlineData(@"N0CALL>WIDE1-1,WIDE2-2:/092345z4903.50N/07201.75W_t068 Testing WX with only temp.", true)]
+    [InlineData(@"N0CALL>WIDE1-1,WIDE2-2:/092345z4903.50N/07201.75W_180/010 Testing WX with only wind direction and speed.", true)]
+    [InlineData(@"N0CALL>WIDE1-1,WIDE2-2:/092345z4903.50N/07201.75W_180h99b0990 Testing WX with only humidity and pressure.", true)]
+    [InlineData(@"N0CALL>WIDE1-1,WIDE2-2:/092345z4903.50N/07201.75W_ Testing WX with no data.", false)]
     [InlineData(@"N0CALL>WIDE1-1,igate,TCPIP*:/092345z4903.50N/07201.75W>Test1234", false)]
     public void IncludeReport(string encodedPacket, bool expectedInclude)
     {
