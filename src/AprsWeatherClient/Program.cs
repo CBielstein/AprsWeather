d054f2f0c15da19f14f5d1a4dce42762cb4d07a1
Cameron Bielstein <CTBielstein@gmail.com>
2023-02-05 23:27:12 -0800

    Code cleanliness refactors on index page (#33)
    Refactors the index page to separate much of the code out of the razor page itself by:
    * Refactoring report list management logic in to new `ReportList.cs` file and object
    * Switching to split `index.razor`/`index.razor.cs` codebehind style to move C# logic code out of razor view code

--- src/AprsWeatherClient/Program.cs
+++ src/AprsWeatherClient/Program.cs
@@ -2,12 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AprsWeatherClient;
 using Darnton.Blazor.DeviceInterop.Geolocation;
+using AprsWeatherClient.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var serverAddress = builder.Configuration["ServerAddress"];
+ArgumentNullException.ThrowIfNull(serverAddress);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(serverAddress) });
 builder.Services.AddScoped<IGeolocationService, GeolocationService>();
+builder.Services.AddScoped<ReportList>();
 
 await builder.Build().RunAsync();
